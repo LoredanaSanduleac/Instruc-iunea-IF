983c3a54e5e0d1d21f39fd810f9f40ab8ff4f6e3
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-10-14 10:53:27 +0300

    Add files via upload

---
+++ Problema_4_IF.py
@@ -0,0 +1,9 @@
+v1=int(input("introduceti varsta primei persoane"))
+v2=int(input("introduceti varsta persoanei a doua"))
+v3=int(input("introduceti varsta persoanei a treia"))
+if ((v1>18)and(v1<60)):
+    print(v1)
+if((v2>18)and(v2<60)):
+    print(v2)
+if ((v3>18)and(v3<60)):
+    print(v3)
