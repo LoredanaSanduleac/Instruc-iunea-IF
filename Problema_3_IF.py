df7216c37f82de761886e6e04060498bb9d6553b
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-10-14 10:48:15 +0300

    Add files via upload

---
+++ Problema_3_IF.py
@@ -0,0 +1,5 @@
+l=str(input("introduceti o litera"))
+if ((l=="o")or(l=="u")or(l=="a")or(l=="e")or(l=="i")or(l=="ă")or(l=="î")or(l=="â")):
+    print("litera",l,"este o vocală")
+else:
+    print("litera",l,"este o consoană")
