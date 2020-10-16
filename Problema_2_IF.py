624d91e108117c3d41eca11ce86014bba8c4072c
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-10-17 00:18:14 +0300

    Update Problema_2_IF.py

--- Problema_2_IF.py
+++ Problema_2_IF.py
@@ -1,3 +1,7 @@
+#La ora de matematică Gigel este scos la tablă. Profesoara îi dictează trei numere şi îi cere să verifice dacă cele trei numere pot fi
+#laturile unui triunghi. Ajutaţi-l pe Gigel să afle rezultatul. Scrieţi un program care primeşte numerele lui Gigel, care sunt mai mici
+#ca 32000, şi returnează DA sau NU. Observaţie: Trei numere pot fi laturile unui triunghi numai dacă fiecare este mai mic ca
+#suma celorlalte două.
 a=int(input("lungimea laturii 1 este: "))
 b=int(input("lungimea laturii 2 este: "))
 c=int(input("lungimea laturii 3 este: "))
