93788d352cf9599ee5a5e6b487477a5a4e4c2abf
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-10-17 00:18:50 +0300

    Update Problema_3_IF.py

--- Problema_3_IF.py
+++ Problema_3_IF.py
@@ -1,3 +1,4 @@
+#Să se verifice dacă o literă introdusă este vocală sau consoană.
 l=str(input("introduceti o litera"))
 if ((l=="o")or(l=="u")or(l=="a")or(l=="e")or(l=="i")or(l=="ă")or(l=="î")or(l=="â")):
     print("litera",l,"este o vocală")
