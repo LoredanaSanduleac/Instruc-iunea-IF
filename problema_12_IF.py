e9f396fad727c590f56e86a627ef4142addbffb6
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-10-17 14:49:42 +0300

    Add files via upload

---
+++ problema_12_IF.py
@@ -0,0 +1,13 @@
+#“Mă iubeşte un pic, mult, cu pasiune, la nebunie, deloc, un pic,...”. Rupând petalele unei margarete cu x petale, el (ea) mă
+#iubeşte ....
+x=int(input("introduceti nr de petale: "))
+if (x%5==0):
+    print("nu ma iubeste deloc")
+if (x%5==1):
+    print("ma iubeste un pic")
+if (x%5==2):
+    print("ma iubeste mult")
+if (x%5==3):
+    print("ma iubeste cu pasiune")
+if (x%5==4):
+    print("ma iubeste la nebunie")
