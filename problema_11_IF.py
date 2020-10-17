ada3119467fff683bf4aede05feb39a3cf72fbbf
Loredana Sanduleac <70658391+LoredanaSanduleac@users.noreply.github.com>
2020-10-17 14:25:15 +0300

    Add files via upload

---
+++ problema_11_IF.py
@@ -0,0 +1,17 @@
+#Se dau trei numere. Să se afişeze aceste numere unul sub altul, afişând în dreptul fiecăruia unul dintre cuvintele PAR sau
+#IMPAR.
+n1=int(input("introduceti nr 1: "))
+n2=int(input("introduceti nr 2: "))
+n3=int(input("introduceti nr 3: "))
+if (n1%2==0):
+    print(n1, "PAR")
+else:
+    print(n1, "IMPAR")
+if (n2%2==0):
+    print(n2, "PAR")
+else: 
+    print(n2, "IMPAR")
+if (n3%2==0):
+    print(n3, "PAR")
+else:
+    print(n3, "IMPAR")
